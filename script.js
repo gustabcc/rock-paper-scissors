76499bb2df0100db8e3793618364760a17281c2c
Gustavo Henrique <gustavo_hmoura@outlook.com>
2023-09-22 23:53:41 -0300

    new change in getComputerChoice

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// this function takes the computer's play.
-const getComputerChoice = function() {
-    const possibleMove = ["rock", "paper", "scissors"]; //list of possible moves.
-    const move = possibleMove[Math.floor(Math.random() * possibleMove.length)] //chose a random move.
+function getComputerChoice() {
+    const moves = ["rock", "paper", "scissors"];
+    const computer = moves[Math.floor(Math.random() * moves.length)];
 
-    return move;
+    return computer;
 }
 
-console.log(getComputerChoice()) 
+console.log(getComputerChoice());
