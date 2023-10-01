40d0591397caf0dbfc06fad98a2ba8f8c593e48f
Gustavo Henrique <gustavo_hmoura@outlook.com>
2023-10-01 12:11:37 -0300

    add rps-ui at project

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const choices = document.querySelectorAll('button');
+const result = document.getElementById('result');
+const score = document.getElementById('score');
+
+let playerScore = 0;
+let computerScore = 0;
+
+choices.forEach(choice => {
+    choice.addEventListener('click', () => {
+        const playerChoice = choice.id;
+        const computerChoice = getComputerChoice();
+        const roundResult = round(playerChoice, computerChoice);
+
+        displayResult(roundResult);
+        updateScore(roundResult);
+    });
+});
+
 function getComputerChoice() {
     const moves = ["rock", "paper", "scissors"];
     const computer = moves[Math.floor(Math.random() * moves.length)];
@@ -6,69 +24,29 @@ function getComputerChoice() {
 }
 
 function round(playerSelection, computerSelection) {
-    if(playerSelection == computerSelection) {
-        return 'Tie'
-    } else if(
-    (playerSelection === 'rock' && computerSelection === 'scissors') ||
-    (playerSelection === 'paper' && computerSelection === 'rock') ||
-    (playerSelection === 'scissors' && computerSelection === 'paper') 
+    if (playerSelection === computerSelection) {
+        return 'Tie';
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
     ) {
-        return 'Player winns';
+        return 'Player wins';
     } else {
-        return 'Computer winns';
+        return 'Computer wins';
     }
 }
 
-function game() {
-
-    let playerScore = 0;
-    let computerScore = 0;
-    let currentRound = 0;
-
-    alert(
-        `
-        Options for choices:
-        1 - rock
-        2 - paper
-        3 - scissors 
-        `
-        )
-
-    for(currentRound; currentRound < 5; currentRound++) {
-        const playerChoice = prompt('rock, paper and scissors??').toLowerCase();
-        const computerChoice = getComputerChoice();
-        const roundResult = round(playerChoice, computerChoice);
-
-        alert(
-            `
-            Round: ${currentRound}
-            Player choice: ${playerChoice}
-            Computer choice: ${computerChoice}
-            Round result: ${roundResult}
-            `
-        )
-    
-        if(roundResult === 'Player winns') {
-            playerScore++;
-        } else if(roundResult === 'Computer winns') {
-            computerScore++;
-        }
-    }
-
-    alert(
-        `
-        Player score: ${playerScore}
-        Computer score: ${computerScore}
-        `
-    )
-
-    if(playerScore > computerScore) {
-        alert('Player is match winner');
-    } else if(playerScore < computerScore) {
-        alert('Computer is match winner');
-    } else {
-        alert('Tie')
-    }
+function displayResult(resultMessage) {
+    result.textContent = resultMessage;
 }
 
-game();
+function updateScore(result) {
+    if (result === 'Player wins') {
+        playerScore++;
+    } else if (result === 'Computer wins') {
+        computerScore++;
+    }
+
+    score.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+}
